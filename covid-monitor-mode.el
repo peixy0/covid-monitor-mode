9d58190b21482c681a788f1abc8f29cc4dd42d7a
peixy0 <pei.xy0@outlook.com>
2021-03-15 16:42:02 +0800

    增加境外输入数据

--- covid-monitor-mode.el
+++ covid-monitor-mode.el
@@ -17,22 +17,55 @@
   "list of provinces to monitor"
   :type '(repeat string))
 
-(defun covid-monitor-format-covid-status-string (country-count province-count-list)
-  (apply 'cl-concatenate 'string (format "COVID-19 全国 %d" country-count)
-         (cl-mapcar (lambda (p count) (format " %s %d" p count))
-                    covid-monitor-province-list province-count-list)))
+(cl-defstruct covid-stat total imported)
 
-(defun covid-monitor-extract-province-data (province-data province)
-  (gethash "totalConfirmed"
-           (cl-find-if (lambda (x) (equal province (gethash "childStatistic" x)))
-                       province-data)))
+(defun covid-monitor-format-covid-stat (covid-stat)
+  (let ((total (covid-stat-total covid-stat))
+        (imported (covid-stat-imported covid-stat)))
+    (if (= 0 imported)
+        (format "%d" total)
+      (format "%d(%d)" total imported))))
+
+(defun covid-monitor-format-covid-status-string (country-stat province-stat-list)
+  (apply 'cl-concatenate 'string (format "COVID-19 全国 %s" (covid-monitor-format-covid-stat country-stat))
+         (cl-mapcar (lambda (p stat) (format " %s %s" p (covid-monitor-format-covid-stat stat)))
+                    covid-monitor-province-list province-stat-list)))
+
+(defun covid-monitor-extract-stat (stat-data)
+  (if (not stat-data) 0
+    (let ((confirmed (gethash "totalConfirmed" stat-data))
+          (doubtful (gethash "totalDoubtful" stat-data))
+          (cured (gethash "totalCured" stat-data))
+          (death (gethash "totalDeath" stat-data)))
+      (- (+ confirmed doubtful) (+ cured death)))))
+
+(defun covid-monitor-find-data (data-list key)
+  (cl-find-if (lambda (x) (equal key (gethash "childStatistic" x)))
+              data-list))
+
+(defun covid-monitor-extract-province-data (province-data-list province)
+  (let* ((province-data (covid-monitor-find-data province-data-list province))
+         (city-data-list (gethash "cityArray" province-data))
+         (imported-data (covid-monitor-find-data city-data-list "境外输入")))
+    (make-covid-stat :total (covid-monitor-extract-stat province-data) :imported (covid-monitor-extract-stat imported-data))))
 
 (defun covid-monitor-extract-province-data-list (raw-data)
-  (cl-mapcar (lambda (x) (covid-monitor-extract-province-data (gethash "provinceArray" raw-data) x))
-          covid-monitor-province-list))
+  (let ((province-data-list (gethash "provinceArray" raw-data)))
+    (cl-mapcar (lambda (x) (covid-monitor-extract-province-data province-data-list x))
+               covid-monitor-province-list)))
+
+(defun covid-monitor-extract-total-imported-stat (province-data-list)
+  (apply '+ (cl-mapcar (lambda (province-data)
+                         (let* ((city-data-list (gethash "cityArray" province-data))
+                                (imported-data (covid-monitor-find-data city-data-list "境外输入")))
+                           (covid-monitor-extract-stat imported-data)))
+                       province-data-list)))
 
 (defun covid-monitor-extract-country-data (raw-data)
-  (gethash "totalConfirmed" (gethash "country" raw-data)))
+  (let* ((country-stat (covid-monitor-extract-stat (gethash "country" raw-data)))
+         (province-data-list (gethash "provinceArray" raw-data))
+         (imported-stat (covid-monitor-extract-total-imported-stat province-data-list)))
+    (make-covid-stat :total country-stat :imported imported-stat)))
 
 (defun covid-monitor-extract-covid-data ()
   (with-current-buffer (get-buffer-create covid-monitor-data-buffer)
@@ -41,19 +74,19 @@
              (json-array-type 'list)
              (json-key-type 'string)
              (covid-raw-data (json-read-from-string (buffer-string)))
-             (country-count (covid-monitor-extract-country-data covid-raw-data))
-             (province-count-list (covid-monitor-extract-province-data-list covid-raw-data)))
-        (setq covid-monitor-status-string (covid-monitor-format-covid-status-string country-count province-count-list))
+             (country-stat (covid-monitor-extract-country-data covid-raw-data))
+             (province-stat-list (covid-monitor-extract-province-data-list covid-raw-data)))
+        (setq covid-monitor-status-string (covid-monitor-format-covid-status-string country-stat province-stat-list))
         (force-mode-line-update 'all)))))
 
-(defun covid-monitor-callback (process event)
+(defun covid-monitor-fetcher-callback (process event)
   (when (not (process-live-p process))
     (let ((exit-code (process-exit-status process)))
       (if (= 0 exit-code)
           (covid-monitor-extract-covid-data)
         (when covid-monitor-require-feedback
           (setq covid-monitor-require-feedback nil)
-          (setq covid-monitor-status-string (format "Fetching COVID-19 data failed: %d" exit-code)))))))
+          (setq covid-monitor-status-string "Failed to fetch COVID-19 data"))))))
 
 (defun covid-monitor-covid-data-update ()
   (with-current-buffer (get-buffer-create covid-monitor-data-buffer)
@@ -63,7 +96,7 @@
                   :command (list "curl" "-s"
                                  "https://ncovdata.market.alicloudapi.com/ncov/cityDiseaseInfoWithTrend"
                                  "-H" (format "Authorization: APPCODE %s" covid-monitor-alicloud-api-appcode))
-                  :sentinel #'covid-monitor-callback)))
+                  :sentinel #'covid-monitor-fetcher-callback)))
 
 (defun covid-monitor-force-update ()
   (interactive)
