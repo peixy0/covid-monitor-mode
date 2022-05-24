2914eaf45024f078aa4f9a0638c77b6ca5e72224
peixy <pei.xy0@outlook.com>
2022-05-24 23:21:20 +0800

    update data source

--- README.md
+++ README.md
@@ -1,25 +1,20 @@
-# 2/7/2022 update 停止维护
-阿里云市场中的疫情数据 API 已停止维护，找到新的可用数据 API 前插件无法使用
-
 # Covid Monitor Mode ![wftpl](http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png)
 
 ![Preview](screenshot.png)
 
+数据来源为丁香园
+
 此插件依赖 `curl`，所以确保系统中 `curl` 命令可以正常使用
 
-首先在阿里云市场上注册申请疫情数据 API 服务（免费）
-https://market.aliyun.com/products/57002003/cmapi00037970.html
-
-找到自己的 APPCODE 设置 `covid-monitor-alicloud-api-appcode`
-
-`covid-monitor-province-list` 用于配置省份
+`covid-monitor-area-list` 用于配置省份或市区
 
 `covid-monitor-update-interval` 用于配置数据更新的间隔时间（单位秒，默认600）
 
 配置示例:
 
-    (use-package covid-monitor-mode
-      :config
-      (setq covid-monitor-alicloud-api-appcode "你的APPCODE")
-      (setq covid-monitor-province-list '("浙江省"))
-      (covid-monitor-mode))
+```
+(use-package covid-monitor-mode
+  :config
+  (setq covid-monitor-area-list '("浙江", "杭州"))
+  (covid-monitor-mode))
+```
--- covid-monitor-mode.el
+++ covid-monitor-mode.el
@@ -1,6 +1,9 @@
 (require 'json)
 
-(defvar covid-monitor-data-buffer "*covid-data*")
+(defvar covid-monitor-data-buffer "*covid-data-raw*")
+(defvar covid-monitor-area-stat-buffer "*covid-area-stat*")
+(defvar covid-monitor-data-anchor-start "try { window.getAreaStat = ")
+(defvar covid-monitor-data-anchor-end "}catch(e){}")
 (defvar covid-monitor-require-feedback t)
 (defvar covid-monitor-status-string "")
 (defvar covid-monitor-update-timer nil)
@@ -9,91 +12,97 @@
   "covid monitor update interval (sec)"
   :type 'integer)
 
-(defcustom covid-monitor-alicloud-api-appcode ""
-  "alicloud appcode for covid data api"
+(defcustom covid-monitor-curl-path "curl"
+  "path for curl binary"
   :type 'string)
 
-(defcustom covid-monitor-province-list '()
-  "list of provinces to monitor"
+(defcustom covid-monitor-area-list '()
+  "list of areas to monitor"
   :type '(repeat string))
 
-(defun covid-monitor-calculate-count (statistics)
-  (if (not statistics) 0
-    (let ((confirmed (gethash "totalConfirmed" statistics))
-          (doubtful (gethash "totalDoubtful" statistics))
-          (cured (gethash "totalCured" statistics))
-          (death (gethash "totalDeath" statistics)))
-      (- (+ confirmed doubtful) (+ cured death)))))
+(defun covid-monitor-extract-area-stat()
+  (goto-char 1)
+  (let* ((anchor-start (search-forward covid-monitor-data-anchor-start))
+         (anchor-end (- (search-forward covid-monitor-data-anchor-end) (length covid-monitor-data-anchor-end))))
+    (copy-region-as-kill anchor-start anchor-end)
+    (with-current-buffer (get-buffer-create covid-monitor-area-stat-buffer)
+      (erase-buffer)
+      (yank))))
 
-(defun covid-monitor-find-child (data-list key)
-  (cl-find-if (lambda (x) (equal key (gethash "childStatistic" x)))
-              data-list))
+(defun covid-monitor-find-city-data (city-list area-name)
+  (cl-find-if (lambda (x) (equal area-name (gethash "cityName" x))) city-list))
 
-(defun covid-monitor-extract-province-imported-count (province-data)
-  (covid-monitor-calculate-count (covid-monitor-find-child (gethash "cityArray" province-data) "境外输入")))
+(defun covid-monitor-find-area-data (province-list area-name)
+  (if (not (eq nil province-list))
+      (let ((area (car province-list))
+            (rest (cdr province-list)))
+        (if (equal area-name (gethash "provinceShortName" area))
+            area
+          (let ((city (covid-monitor-find-city-data (gethash "cities" area) area-name)))
+            (if (not (eq nil city))
+                city
+              (covid-monitor-find-area-data rest area-name)))))))
 
-(defun covid-monitor-extract-total-imported-count (covid-raw-data)
-  (apply '+ (cl-mapcar 'covid-monitor-extract-province-imported-count
-                       (gethash "provinceArray" covid-raw-data))))
+(defun covid-monitor-process-json-data (json-data area-name)
+  (let* ((area (covid-monitor-find-area-data json-data area-name))
+         (cities (gethash "cities" area))
+         (imported (covid-monitor-find-city-data cities "境外输入")))
+    (puthash "areaName" area-name area)
+    (if (not (eq nil imported))
+        (puthash "currentImportedCount" (gethash "currentConfirmedCount" imported) area))
+    area))
 
-(defun covid-monitor-format-imported-count (imported-count)
-  (if (= 0 imported-count) ""
-    (format "(%d)" imported-count)))
+(defun covid-monitor-process-raw-data ()
+  (let* ((json-object-type 'hash-table)
+         (json-array-type 'list)
+         (json-key-type 'string)
+         (extracted-json (json-read-from-string (buffer-string))))
+    (cl-mapcar (lambda (x) (covid-monitor-process-json-data extracted-json x)) covid-monitor-area-list)))
 
-(defun covid-monitor-format-total-data (covid-raw-data)
-  (format " %d%s"
-          (covid-monitor-calculate-count (gethash "country" covid-raw-data))
-          (covid-monitor-format-imported-count (covid-monitor-extract-total-imported-count covid-raw-data))))
+(defun covid-monitor-format-area-data (area)
+  (let ((name (gethash "areaName" area))
+        (currentConfirmed (gethash "currentConfirmedCount" area))
+        (imported (gethash "currentImportedCount" area)))
+    (if (eq nil imported)
+        (format "%s %d" name currentConfirmed)
+      (format "%s %d(%d)" name currentConfirmed imported))))
 
-(defun covid-monitor-format-province-data (province-data)
-  (format " %s %d%s"
-          (gethash "childStatistic" province-data)
-          (covid-monitor-calculate-count province-data)
-          (covid-monitor-format-imported-count (covid-monitor-extract-province-imported-count province-data))))
+(defun covid-monitor-format-message (area-list)
+  (format "COVID-19 %s "
+          (string-join (cl-mapcar #'covid-monitor-format-area-data area-list) " ")))
 
-(defun covid-monitor-extract-province-data-list (covid-raw-data)
-  (let ((province-data-list (gethash "provinceArray" covid-raw-data)))
-    (cl-mapcar (lambda (x) (covid-monitor-find-child province-data-list x)) covid-monitor-province-list)))
-
-(defun covid-monitor-format-covid-status-string (covid-raw-data)
-  (let ((province-data-list (covid-monitor-extract-province-data-list covid-raw-data)))
-    (apply 'cl-concatenate 'string "COVID-19 全国" (covid-monitor-format-total-data covid-raw-data)
-           (cl-mapcar 'covid-monitor-format-province-data province-data-list))))
-
-(defun covid-monitor-extract-covid-data ()
+(defun covid-monitor-process-data ()
   (with-current-buffer (get-buffer-create covid-monitor-data-buffer)
     (when (< 0 (length (buffer-string)))
-      (let* ((json-object-type 'hash-table)
-             (json-array-type 'list)
-             (json-key-type 'string)
-             (covid-raw-data (json-read-from-string (buffer-string))))
-        (setq covid-monitor-status-string (covid-monitor-format-covid-status-string covid-raw-data))
-        (force-mode-line-update 'all)))))
+      (covid-monitor-extract-area-stat)))
+  (with-current-buffer (get-buffer-create covid-monitor-area-stat-buffer)
+    (setq covid-monitor-status-string
+          (covid-monitor-format-message (covid-monitor-process-raw-data))))
+  (force-mode-line-update 'all))
 
-(defun covid-monitor-fetcher-callback (process event)
+(defun covid-monitor-fetch-data-callback (process event)
   (when (not (process-live-p process))
     (let ((exit-code (process-exit-status process)))
       (if (= 0 exit-code)
-          (covid-monitor-extract-covid-data)
+          (covid-monitor-process-data)
         (when covid-monitor-require-feedback
           (setq covid-monitor-require-feedback nil)
-          (setq covid-monitor-status-string "Failed to fetch COVID-19 data"))))))
+          (setq covid-monitor-status-string "Failed to fetch COVID-19 data "))))))
 
-(defun covid-monitor-covid-data-update ()
+(defun covid-monitor-fetch-data ()
   (with-current-buffer (get-buffer-create covid-monitor-data-buffer)
     (erase-buffer)
     (make-process :name "covid-data-fetcher"
                   :buffer covid-monitor-data-buffer
-                  :command (list "curl" "-s"
-                                 "https://ncovdata.market.alicloudapi.com/ncov/cityDiseaseInfoWithTrend"
-                                 "-H" (format "Authorization: APPCODE %s" covid-monitor-alicloud-api-appcode))
-                  :sentinel #'covid-monitor-fetcher-callback)))
+                  :command (list covid-monitor-curl-path "-s"
+                                 "https://ncov.dxy.cn/ncovh5/view/pneumonia")
+                  :sentinel #'covid-monitor-fetch-data-callback)))
 
 (defun covid-monitor-force-update ()
   (interactive)
-  (setq covid-monitor-status-string "Fetching COVID-19 data...")
+  (setq covid-monitor-status-string "Fetching COVID-19 data... ")
   (setq covid-monitor-require-feedback t)
-  (covid-monitor-covid-data-update))
+  (covid-monitor-fetch-data))
 
 (define-minor-mode covid-monitor-mode
   "covid-monitor-mode"
@@ -103,12 +112,12 @@
   (setq covid-monitor-status-string "")
   (or global-mode-string (setq global-mode-string '("")))
   (when covid-monitor-mode
-    (setq covid-monitor-status-string "Fetching COVID-19 data...")
+    (setq covid-monitor-status-string "Fetching COVID-19 data... ")
     (or (memq 'covid-monitor-status-string global-mode-string)
         (setq global-mode-string
               (append global-mode-string '(covid-monitor-status-string))))
     (setq covid-monitor-update-timer
           (run-at-time "0 sec" covid-monitor-update-interval
-                       'covid-monitor-covid-data-update))))
+                       'covid-monitor-fetch-data))))
 
 (provide 'covid-monitor-mode)
